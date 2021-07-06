6f795c008b22b065c20ffe836eea8bc13977b91e
Hadi N <hadinurdin99@gamil.com>
2021-07-06 16:07:20 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LaunchListComponent } from './components/launch-list/launch-list.component';
+import { LaunchDetailsComponent } from './components/launch-details/launch-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchListComponent,
+    LaunchDetailsComponent
   ],
   imports: [
     BrowserModule,
