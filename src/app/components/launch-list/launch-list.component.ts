6107e40bd0cc59a7c859ba5b1e03b32f45824b90
Hadi N <hadinurdin99@gamil.com>
2021-07-07 14:36:47 +0700

    launch list components

--- src/app/components/launch-list/launch-list.component.ts
+++ src/app/components/launch-list/launch-list.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { PastLaunchesListGQL } from 'src/app/services/spacexGraphql.service';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-launch-list',
   templateUrl: './launch-list.component.html',
-  styleUrls: ['./launch-list.component.css']
+  styleUrls: ['./launch-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LaunchListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pastLaunchService: PastLaunchesListGQL) { }
+
+  //limit fetch
+  pastLaunches$ = this.pastLaunchService
+  .fetch({ limit: 30 })
+
+  //extract query data
+  .pipe(
+    map(
+      res => res.data.launchesPast
+    )
+  );
 
   ngOnInit(): void {
   }
